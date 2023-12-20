2194597130075a5fc24967d1af84f70e507afb06
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-12-20 14:05:35 +0700

    setup service read file csv

--- src/main/java/com/application/scraper/BinanceScraper.java
+++ src/main/java/com/application/scraper/BinanceScraper.java
@@ -1,5 +1,5 @@
 package com.application.scraper;
 
 public class BinanceScraper {
-
+	
 }
--- src/main/java/com/application/scraper/ScraperMain.java
+++ src/main/java/com/application/scraper/ScraperMain.java
@@ -1,11 +1,30 @@
 package com.application.scraper;
 
+import java.util.List;
+
+import com.application.App;
+import com.application.service.CSVFileManager;
+
 public class ScraperMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello world");
-
+		CSVFileManager filer = new CSVFileManager(String.valueOf(App.class.getResource("data/tweets/tweet-17-12-2023.csv")).substring(6));
+		
+		List<String[]> data = filer.readData();
+		System.out.print(data);
+		
+		for (String[] item: data) {
+			 String id = item[0];
+             String urlTweet = item[1];
+             String contentTweet = item[2];
+             String tweetDate = item[3];
+             String commentTweet = item[4];
+             String reTweet = item[5];
+             String quoteTweet = item[6];
+             String heartTweet = item[7];
+             
+             System.out.println(id);
+		}
 	}
 
 }
